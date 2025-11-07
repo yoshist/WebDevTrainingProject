3a19c97b674a3b9adfc831b976d11968f5f19413
yoshist <stacy.yoshimitsu@gmail.com>
2025-11-07 08:51:51 -0800

    removed bad reference

--- scouting-app/src/main.jsx
+++ scouting-app/src/main.jsx
@@ -3,14 +3,11 @@ import { createRoot } from 'react-dom/client'
 import { HashRouter } from 'react-router-dom';
 import './index.css'
 import App from './App.jsx'
-import { AppProvider } from '../../Module16.6_Bad_Bank/bad-bank/src/context/userContext.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <HashRouter>
-      <AppProvider>
         <App />
-      </AppProvider>
     </HashRouter>
   </StrictMode>,
 )
