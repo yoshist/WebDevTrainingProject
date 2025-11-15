4e0d7b9384820e48e48581da7b8e49c49298f4c5
yoshist <stacy.yoshimitsu@gmail.com>
2025-11-15 12:17:11 -0800

    Railey's change to add dtf form

--- scouting-app/src/routes/dtf.jsx
+++ scouting-app/src/routes/dtf.jsx
@@ -1,7 +1,40 @@
-import React from 'react'
+import React from "react";
 
 export default function Dtf() {
   return (
-    <div>dtf</div>
-  )
+    <div>
+      <h1> Drive Team Feeder </h1>
+      <h2> Alliance 1 </h2>
+      <form>
+        <label> Team 1 Number</label>
+        <div>
+          <input type="number" className="input" />
+        </div>
+        <label> Team 2 Number</label>
+        <div>
+          <input type="number" className="input" />
+        </div>
+        <label> Team 3 Number</label>
+        <div>
+          <input type="number" className="input" />
+        </div>
+        <h2> Alliance 2 </h2>
+        <label> Team 1 Number</label>
+        <div>
+          <input type="number" className="input" />
+        </div>
+        <label> Team 2 Number</label>
+        <div>
+          <input type="number" className="input" />
+        </div>
+        <label> Team 3 Number</label>
+        <div>
+          <input type="number" className="input" />
+        </div>
+      </form>
+      <h3>
+        <button>SUBMIT</button>
+      </h3>
+    </div>
+  );
 }
